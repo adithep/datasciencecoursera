3744f1c9ee708af808f20dba73435f97a517828f
Adithep <badsxx@gmail.com>
2014-10-24 00:35:06 +0700

    k

--- cleandata/project/run_analysis.R
+++ cleandata/project/run_analysis.R
@@ -1,4 +1,4 @@
-pkg <- c("dplyr", "reshape2")
+pkg <- c("dplyr", "tidyr")
 lapply(pkg, require, character.only=T)
 
 to_dplyr <- function(f) {
@@ -15,7 +15,11 @@ y_test <- read.table(file.path(data_path, "test", "y_test.txt"))
 x_train <- to_dplyr(file.path(data_path, "train", "X_train.txt"))
 x_test <- to_dplyr(file.path(data_path, "test", "X_test.txt"))
 features <- to_dplyr(file.path(data_path, "features.txt"))
+act_names <- to_dplyr(file.path(data_path, "activity_labels.txt"))
 
+
+act_names<-act_names %>%
+  rename(activity_num=V1, activity_name=V2)
 features<-features %>%
   rename(feature_num=V1, feature_name=V2)%>%
   filter(feature_name=grepl("mean\\(\\)|std\\(\\)", feature_name))%>%
@@ -25,5 +29,8 @@ y <- rbind(y_train, y_test)
 x <- rbind_list(x_train, x_test)
 dt<-x %>%
   select(one_of(features$feature_num)) %>%
-  mutate(subjects=as.double(subjects$V1), activity_num=as.double(y$V1)) %>%
-  arrange(subjects, activity_num)
+  mutate(subjects=as.double(subjects$V1), activity_num=as.double(y$V1))
+dt<-left_join(dt, act_names, by="activity_num")
+dt<-dt %>%
+  group_by(subjects, activity_num, activity_name)%>%
+  gather()
